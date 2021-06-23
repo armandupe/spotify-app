16abe23b4d119a556c28f5ebe86f8aae1a91989b
Dmitrii Kudriavtsev <symbolic28@gmail.com>
2021-06-23 10:36:01 +0300

    basic functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
-const HASH = window.location.hash
+import "../css/style.css";
+
+const hash = window.location.hash
   .substring(1)
   .split("&")
   .reduce(function (initial, item) {
@@ -21,24 +23,69 @@ if (!TOKEN) {
   window.location = `${AUTH_END_POINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES.join("%20")}&response_type=token&show_dialog=true`;
 }
 
-const LIMIT = 10;
-const TIME_RANGE = "long_term";
-const OFFSET = 0;
-const URI = `https://api.spotify.com/v1/me/top/artists?time_range=${TIME_RANGE}&limit=${LIMIT}&offset=${OFFSET}`;
+const queryOptionsForm = document.querySelector(".queryOptionsForm");
+queryOptionsForm.addEventListener("click", (e) => {
+  const TIME_RANGE = e.target.value;
+  const LIMIT = 10;
+  const OFFSET = 0;
+  const URI = `https://api.spotify.com/v1/me/top/artists?time_range=${TIME_RANGE}&limit=${LIMIT}&offset=${OFFSET}`;
 
-async function getData() {
-  await fetch(`${URI}`, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((response) => {
-      console.log(response);
-    });
-}
+  async function getData() {
+    await fetch(`${URI}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        const main = document.createElement("main");
 
-console.log(getData());
+        response.items.forEach((item) => {
+          let artistName = item.name;
+          let artistImages = item.images;
+          let artistPopularity = item.popularity;
+          let artistLink = item.external_urls.spotify;
+          let artistFollowers = `${item.followers.total} followers`;
+          let artistsGenres = item.genres;
+
+          const section = document.createElement("section");
+          const ul = document.createElement("ul");
+
+          artistsGenres.forEach((genre) => {
+            let artistGenre = genre;
+            const li = document.createElement("li");
+            li.textContent = artistGenre;
+            ul.insertAdjacentElement("afterbegin", li);
+            section.insertAdjacentElement("beforeend", ul);
+          });
+
+          let fragment = document.createDocumentFragment();
+
+          const image = document.createElement("img");
+          image.src = artistImages[1].url;
+          section.insertAdjacentElement("afterbegin", image);
+
+          const link = document.createElement("a");
+          const title = document.createElement("h2");
+          link.href = artistLink;
+          title.textContent = artistName;
+          link.insertAdjacentElement("afterbegin", title);
+          section.insertAdjacentElement("afterbegin", link);
+
+          const followers = document.createElement("span");
+          followers.textContent = artistFollowers;
+          section.insertAdjacentElement("beforeend", followers);
+
+          main.insertAdjacentElement("afterbegin", section);
+          fragment.appendChild(main);
+
+          document.body.appendChild(fragment);
+        });
+      });
+  }
+
+  getData();
+});
